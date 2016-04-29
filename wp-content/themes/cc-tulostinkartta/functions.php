16703511ef98794793a1554f50eae0ee5b1d7b93
Tomi Toivio <tomi@sange.fi>
2016-04-30 02:06:52 +0300

    Tähän versioon ei tule PDF-laskujen lähettämistä.

--- wp-content/themes/cc-tulostinkartta/functions.php
+++ wp-content/themes/cc-tulostinkartta/functions.php
@@ -8,16 +8,4 @@ wp_enqueue_style( 'custom-community', get_template_directory_uri() . '/style.css
 @ini_set( 'post_max_size', '64M');
 @ini_set( 'max_execution_time', '300' );
 
-/*
-add_filter ( 'pdf_content_additional_content' , 'pdf_additional_content_terms' );
-function pdf_additional_content_terms( $content ) {
-global $woocommerce;
-$tilausnumero = $order_id;
-$tilausnumero = substr($tilausnumero, 4);
-$viitenumero = get_post_meta($tilausnumero, "viitenumero", true);
-$content = str_replace( '[[PDFVIITENUMERO]]', $viitenumero, $content );
-return $content;
-}
-*/
-
 ?>
