4c9c9591a5b88ade23717b4fac205edcb0b6c8ab
Tomi Toivio <tomi@sange.fi>
2016-04-30 03:43:54 +0300

    Advanced Custom Forms fields for Tulostinkartta

--- wp-content/themes/cc-tulostinkartta/functions.php
+++ wp-content/themes/cc-tulostinkartta/functions.php
@@ -10,6 +10,7 @@ wp_enqueue_style( 'custom-community', get_template_directory_uri() . '/style.css
 
 /* Advanced Custom Forms fields for Tulostinkartta */
 
+
 /*
 if( function_exists('acf_add_local_field_group') ):
 
